f147d730cd5a2fd750d8881f4db1481e310cda4b
Maxwell Gover <maxwellrgover@gmail.com>
2017-02-03 19:34:54 -0500

    Updated FooterTabs

---
+++ app/containers/FooterTabs/FooterTabsContainer.js
@@ -0,0 +1,13 @@
+import React, { PropTypes, Component } from 'react'
+import { View, Text } from 'react-native'
+import { Splash } from '~/components'
+import { FooterTabs } from '~/components'
+
+export default class FooterTabsContainer extends Component {
+	
+	render () {
+		return (
+			<FooterTabs />
+		)
+	}
+}
